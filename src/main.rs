18614efe26a9e37a18124eed091d20846d77a19c
Ryan Devenney <rwdevenney@gmail.com>
2022-05-01 14:40:57 -0400

    echo server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::{net::TcpListener, io::{AsyncWriteExt, AsyncReadExt}};
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("localhost:8080").await.unwrap();
+
+    let (mut socket, addr) = listener.accept().await.unwrap();
+
+    let mut buffer = [0u8; 1024];
+
+    let bytes_read = socket.read(&mut buffer).await.unwrap();
+
+    socket.write_all(&buffer[..bytes_read]).await.unwrap();    
 }
