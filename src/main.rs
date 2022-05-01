ff7097040ccc3c415cab009663f717705a3da6e0
Ryan Devenney <rwdevenney@gmail.com>
2022-05-01 16:02:12 -0400

    select

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 use tokio::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
-    net::TcpListener,
+    net::TcpListener, sync::broadcast,
 };
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
-    loop {
-        let (mut socket, _addr) = listener.accept().await.unwrap();
+    let (tx, _rx) = broadcast::channel(10);
     
+    loop {
+        let (mut socket, addr) = listener.accept().await.unwrap();
+        
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
+
         tokio::spawn(async move {
             let (read, mut writer) = socket.split();
         
@@ -18,15 +23,23 @@ async fn main() {
             let mut line = String::new();
         
             loop {
-                let bytes_read = reader.read_line(&mut line).await.unwrap();
-        
-                if bytes_read == 0 {
-                    break;
+                tokio::select! {
+                    result = reader.read_line(&mut line) => {
+                        if result.unwrap() == 0 {
+                            break;
+                        }
+
+                        tx.send((line.clone(), addr)).unwrap();
+                        line.clear();
+                    }
+                    result = rx.recv() => {
+                        let (msg, other_addr) = result.unwrap();
+
+                        if addr != other_addr {
+                            writer.write_all(msg.as_bytes()).await.unwrap();
+                        }
+                    }
                 }
-        
-                writer.write_all(&line.as_bytes()).await.unwrap();
-        
-                line.clear();
             }
         });
     }
